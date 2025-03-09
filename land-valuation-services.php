faf10efb0059632429286e4d205266f90e555fc0
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-09 05:49:34 +0530

    Update land-valuation-services.php

--- land-valuation-services.php
+++ land-valuation-services.php
@@ -89,14 +89,14 @@ include 'includes/config.php';
 
                 </div>
                 <div class="col-md-6">
-                    <img src="img/land/1.jpg" class="img-fluid">
+                    <img src="img/land/1.jpg" class="img-fluid" alt="Agriculture Valuer & Appraiser in Mumbai">
                 </div>
             </div>
 
             
             <div class="row mt-5 mb-5">
                 <div class="col-md-6">
-                    <img src="img/land/2.jpg" class="img-fluid">
+                    <img src="img/land/2.jpg" class="img-fluid" alt="Female agronomist using smart technology for farm management.">
                 </div>
                 <div class="col-md-6">
                     <h2>Agricultural Land Valuation</h2>
@@ -146,7 +146,7 @@ include 'includes/config.php';
                         <p>✔ Timely & Professional Service – Fast, reliable, and unbiased agricultural assessments.</p>
                 </div>
                 <div class="col-md-6">
-                    <img src="img/land/3.jpg" class="img-fluid">
+                    <img src="img/land/3.jpg" class="img-fluid" alt="Understanding of Land Valuation">
                 </div>
             </div>
 
